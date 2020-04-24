b87721e6dc14624cac8fd0c06322b2579ebc55a7
Ville Kokkarinen <kokkarinen.ville@gmail.com>
2020-04-24 13:15:51 +0300

    asd

--- UI/Form2.Designer.cs
+++ UI/Form2.Designer.cs
@@ -39,66 +39,85 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.Clb_Ominaisuudet = new System.Windows.Forms.CheckedListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.combobox_Loan_ItemType = new System.Windows.Forms.ComboBox();
+            this.txt_LoanItem_SearchItem = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt_Loan_ReservedTill = new System.Windows.Forms.TextBox();
+            this.txt_Loan_Item_State = new System.Windows.Forms.TextBox();
+            this.txt_Loan_ItemType = new System.Windows.Forms.TextBox();
+            this.checklist_Loan_Items = new System.Windows.Forms.CheckedListBox();
             this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.checklist_Loan_Items);
             this.panel1.Controls.Add(this.listBox1);
-            this.panel1.Location = new System.Drawing.Point(319, 0);
+            this.panel1.Location = new System.Drawing.Point(239, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(859, 615);
+            this.panel1.Size = new System.Drawing.Size(644, 500);
             this.panel1.TabIndex = 0;
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(287, 284);
+            this.listBox1.Location = new System.Drawing.Point(435, 355);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(264, 164);
+            this.listBox1.Size = new System.Drawing.Size(199, 134);
             this.listBox1.TabIndex = 0;
             // 
             // Cmb_Alue
             // 
             this.Cmb_Alue.FormattingEnabled = true;
-            this.Cmb_Alue.Location = new System.Drawing.Point(100, 17);
+            this.Cmb_Alue.Location = new System.Drawing.Point(75, 14);
+            this.Cmb_Alue.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Cmb_Alue.Name = "Cmb_Alue";
-            this.Cmb_Alue.Size = new System.Drawing.Size(121, 24);
+            this.Cmb_Alue.Size = new System.Drawing.Size(92, 21);
             this.Cmb_Alue.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(30, 20);
+            this.label1.Location = new System.Drawing.Point(22, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.Size = new System.Drawing.Size(28, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Alue";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(100, 80);
+            this.dateTimePicker1.Location = new System.Drawing.Point(75, 65);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(151, 20);
             this.dateTimePicker1.TabIndex = 3;
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(100, 131);
+            this.dateTimePicker2.Location = new System.Drawing.Point(75, 106);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.Size = new System.Drawing.Size(151, 20);
             this.dateTimePicker2.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Location = new System.Drawing.Point(9, 69);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.Size = new System.Drawing.Size(54, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Tulopäivä";
             // 
@@ -106,43 +125,133 @@
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(12, 136);
+            this.label3.Location = new System.Drawing.Point(9, 110);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 17);
+            this.label3.Size = new System.Drawing.Size(60, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Lähtöpäivä";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(113, 183);
+            this.comboBox1.Location = new System.Drawing.Point(85, 149);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
             this.comboBox1.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(12, 186);
+            this.label4.Location = new System.Drawing.Point(9, 151);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(95, 17);
+            this.label4.Size = new System.Drawing.Size(72, 13);
             this.label4.TabIndex = 8;
             this.label4.Text = "Henkilömäärä";
             // 
             // Clb_Ominaisuudet
             // 
             this.Clb_Ominaisuudet.FormattingEnabled = true;
-            this.Clb_Ominaisuudet.Location = new System.Drawing.Point(33, 275);
+            this.Clb_Ominaisuudet.Location = new System.Drawing.Point(25, 223);
+            this.Clb_Ominaisuudet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Clb_Ominaisuudet.Name = "Clb_Ominaisuudet";
-            this.Clb_Ominaisuudet.Size = new System.Drawing.Size(120, 89);
+            this.Clb_Ominaisuudet.Size = new System.Drawing.Size(91, 64);
             this.Clb_Ominaisuudet.TabIndex = 9;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.combobox_Loan_ItemType);
+            this.groupBox3.Controls.Add(this.txt_LoanItem_SearchItem);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox3.Location = new System.Drawing.Point(243, 18);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(187, 99);
+            this.groupBox3.TabIndex = 52;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Hae Tavaroita";
+            // 
+            // combobox_Loan_ItemType
+            // 
+            this.combobox_Loan_ItemType.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.combobox_Loan_ItemType.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.combobox_Loan_ItemType.DisplayMember = "Text";
+            this.combobox_Loan_ItemType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.combobox_Loan_ItemType.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.combobox_Loan_ItemType.FormattingEnabled = true;
+            this.combobox_Loan_ItemType.Location = new System.Drawing.Point(7, 58);
+            this.combobox_Loan_ItemType.Name = "combobox_Loan_ItemType";
+            this.combobox_Loan_ItemType.Size = new System.Drawing.Size(140, 28);
+            this.combobox_Loan_ItemType.TabIndex = 47;
+            // 
+            // txt_LoanItem_SearchItem
+            // 
+            this.txt_LoanItem_SearchItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txt_LoanItem_SearchItem.Location = new System.Drawing.Point(7, 23);
+            this.txt_LoanItem_SearchItem.Name = "txt_LoanItem_SearchItem";
+            this.txt_LoanItem_SearchItem.Size = new System.Drawing.Size(140, 26);
+            this.txt_LoanItem_SearchItem.TabIndex = 45;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txt_Loan_ReservedTill);
+            this.groupBox2.Controls.Add(this.txt_Loan_Item_State);
+            this.groupBox2.Controls.Add(this.txt_Loan_ItemType);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox2.Location = new System.Drawing.Point(244, 165);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(186, 124);
+            this.groupBox2.TabIndex = 51;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tavaran tiedot";
+            // 
+            // txt_Loan_ReservedTill
+            // 
+            this.txt_Loan_ReservedTill.Location = new System.Drawing.Point(6, 92);
+            this.txt_Loan_ReservedTill.Name = "txt_Loan_ReservedTill";
+            this.txt_Loan_ReservedTill.ReadOnly = true;
+            this.txt_Loan_ReservedTill.Size = new System.Drawing.Size(172, 26);
+            this.txt_Loan_ReservedTill.TabIndex = 3;
+            // 
+            // txt_Loan_Item_State
+            // 
+            this.txt_Loan_Item_State.Location = new System.Drawing.Point(6, 59);
+            this.txt_Loan_Item_State.Name = "txt_Loan_Item_State";
+            this.txt_Loan_Item_State.ReadOnly = true;
+            this.txt_Loan_Item_State.Size = new System.Drawing.Size(143, 26);
+            this.txt_Loan_Item_State.TabIndex = 2;
+            // 
+            // txt_Loan_ItemType
+            // 
+            this.txt_Loan_ItemType.Location = new System.Drawing.Point(6, 25);
+            this.txt_Loan_ItemType.Name = "txt_Loan_ItemType";
+            this.txt_Loan_ItemType.ReadOnly = true;
+            this.txt_Loan_ItemType.Size = new System.Drawing.Size(144, 26);
+            this.txt_Loan_ItemType.TabIndex = 1;
+            // 
+            // checklist_Loan_Items
+            // 
+            this.checklist_Loan_Items.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.checklist_Loan_Items.CheckOnClick = true;
+            this.checklist_Loan_Items.DisplayMember = "Text";
+            this.checklist_Loan_Items.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.checklist_Loan_Items.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.checklist_Loan_Items.FormattingEnabled = true;
+            this.checklist_Loan_Items.HorizontalScrollbar = true;
+            this.checklist_Loan_Items.Location = new System.Drawing.Point(14, 14);
+            this.checklist_Loan_Items.Name = "checklist_Loan_Items";
+            this.checklist_Loan_Items.Size = new System.Drawing.Size(225, 276);
+            this.checklist_Loan_Items.TabIndex = 50;
             // 
             // Varaus
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1178, 616);
+            this.ClientSize = new System.Drawing.Size(884, 500);
             this.Controls.Add(this.Clb_Ominaisuudet);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.comboBox1);
@@ -153,10 +262,15 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dateTimePicker1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Varaus";
             this.Text = "Varaus";
             this.Load += new System.EventHandler(this.Varaus_Load);
             this.panel1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,5 +289,13 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.CheckedListBox Clb_Ominaisuudet;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox combobox_Loan_ItemType;
+        private System.Windows.Forms.TextBox txt_LoanItem_SearchItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_Loan_ReservedTill;
+        private System.Windows.Forms.TextBox txt_Loan_Item_State;
+        private System.Windows.Forms.TextBox txt_Loan_ItemType;
+        private System.Windows.Forms.CheckedListBox checklist_Loan_Items;
     }
 }
